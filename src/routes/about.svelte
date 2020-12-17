ab11f2aac84b34b27fd2367d5d87b1808d629f7d
Carlo Berardelli <owner@Carlos-MBP.fios-router.home>
2020-12-16 19:30:13 -0500

    humble beginnings

---
+++ src/routes/about.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About Be The Change</h1>
+
+<p>Be the change is a platform where users can easily find volunteer opportunities in their area and filter the results by opportunity type. Organizations and individuals can register to post volunteer opportunites that they will be hosting</p>
+
+<p>“Constantly think of the universe as a single living being, comprised of a single substance and a single soul; and how all things issue into the single perception of this being, and how it accomplishes all things through a single impulse; and how all things work together to cause all that comes to be, and how intricate and densely woven is the fabric formed by their interweaving” – Marcus Aurelius, Meditations, 4.40</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,54 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+</script>
+
+<style>
+	* {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Be The Change</title>
+</svelte:head>
+
+<h1>Be The Change</h1>
+
+<figure>
+	<img alt="Success Kid" src="{successkid}">
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<div>
+	<h2>Volunteering Opportunites in the 412</h2>
+	<p>Find your next volunteering gig here</p>
+	<a href="opportunities">View Opportunities</a>
+</div>
